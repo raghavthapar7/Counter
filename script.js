d9f15b9293620075195334e91220d566728c1dad
Raghav Thapar <raghavthapar.apps@gmail.com>
2023-08-19 12:48:38 +0530

    Added the decrease counter fucntionality

--- script.js
+++ script.js
@@ -2,12 +2,22 @@
 
 let increaseBtn = document.querySelector(".increase");
 let counter = document.querySelector(".counter");
+let decreaseBtn = document.querySelector(".decrease");
 // console.log(counter);
 let counterNumber = 0;
 
-//Increase the number functionality
+// Increase the number functionality
 increaseBtn.addEventListener("click", function () {
   counterNumber++;
   counter.textContent = counterNumber;
+  counter.classList.remove("counter-decrease");
   counter.classList.add("counter-increase");
+});
+
+// Decrease the number functionality
+decreaseBtn.addEventListener("click", function () {
+  counterNumber--;
+  counter.textContent = counterNumber;
+  counter.classList.remove("counter-increase");
+  counter.classList.add("counter-decrease");
 });
